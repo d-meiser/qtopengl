249456d26db9a99e8bacc9a91f71b8db83ef9ba2
Dominic Meiser <dmeiser79@gmail.com>
2016-08-04 20:01:55 -0600

    Add more meaningful OpenGL testing.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <QOpenGLContext>
+#include <QOffscreenSurface>
+#include <QOpenGLFunctions>
+
+
+struct MyRenderer : protected QOpenGLFunctions {
+    void initGL() {
+        initializeOpenGLFunctions();
+    }
+};
+
 
 int main(int argn, char** argv) {
     Q_UNUSED(argn);
     Q_UNUSED(argv);
+
+    QOpenGLContext context;
+    context.create();
+    QOffscreenSurface surface;
+    surface.create();
+    context.makeCurrent(&surface);
+
     return 0;
 }
