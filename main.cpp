9616190ccb5073ebf7b0213623f7b23ee3989eed
Dominic Meiser <dmeiser79@gmail.com>
2016-08-04 20:42:31 -0600

    Fix context initialization.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,34 @@
+#include <QGuiApplication>
 #include <QOpenGLContext>
 #include <QOffscreenSurface>
-#include <QOpenGLFunctions>
-
-
-struct MyRenderer : protected QOpenGLFunctions {
-    void initGL() {
-        initializeOpenGLFunctions();
-    }
-};
+#include <QSurfaceFormat>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QDebug>
 
 
 int main(int argn, char** argv) {
-    Q_UNUSED(argn);
-    Q_UNUSED(argv);
+    QGuiApplication app(argn, argv);
+
+    QSurfaceFormat format;
+    format.setMajorVersion(3);
+    format.setMinorVersion(3);
 
     QOpenGLContext context;
+    context.setFormat(format);
     context.create();
+    if (!context.isValid()) return -1;
+
     QOffscreenSurface surface;
+    surface.setFormat(format);
     surface.create();
+    if(!surface.isValid()) return -2;
+
     context.makeCurrent(&surface);
+
+    QOpenGLFunctions_3_3_Core f;
+    if(!f.initializeOpenGLFunctions()) return -3;
+
+    qDebug() << QString::fromLatin1((const char*)f.glGetString(GL_VERSION));
 
     return 0;
 }
